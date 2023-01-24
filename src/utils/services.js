fd645d7c4f5fdaca5a1db064878d3212774430aa
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2023-01-23 19:53:58 -0500

    changed api key back to old cause new one sucks

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,15 +1,15 @@
 const mainSearch = async (url) => {
-    const response = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + url + "&appid=437797cffc2e8d40f05daee937e35763");
+    const response = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + url + "&appid=7f797aeb2c4333e090bbfef3ca363921");
     const latlong = await response.json();
 
     return altSearch(latlong.city.coord.lat, latlong.city.coord.lon);
 }
 
 const altSearch = async (lat, long) => {
-    const mainResponse = await fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=437797cffc2e8d40f05daee937e35763");
+    const mainResponse = await fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&appid=7f797aeb2c4333e090bbfef3ca363921");
     const weather = await mainResponse.json();
 
-    const altResponse = await fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&units=imperial&appid=437797cffc2e8d40f05daee937e35763");
+    const altResponse = await fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&units=imperial&appid=7f797aeb2c4333e090bbfef3ca363921");
     const fiveDay = await altResponse.json();
 
     return {weather, fiveDay};
